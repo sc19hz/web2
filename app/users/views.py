d6935d9f5c7bf70da66be665aa899af4680e8975
sc19hz <sc19hz@leeds.ac.uk>
2021-12-04 15:46:00 +0800

    v1.0：follow with no comment

--- app/users/views.py
+++ app/users/views.py
@@ -43,27 +43,20 @@ def followers(username):
     pagination = user.followers.paginate(
         page, per_page=current_app.config['FOLLOWERS_PER_PAGE'],
         error_out=False)
-    follows = [{'user': item.follower, 'timestamp': item.timestamp}
-                for item in pagination.items]
-    return render_template('followers.html', user=user, title="Followers of",
-                            endpoint='.followers', pagination=pagination,
-                            follows=follows)
-
-@users.route('/followed_by/<username>')
-def followed_by(username):
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        flash('Invalid user.')
-        return redirect(url_for('.index'))
-    page = request.args.get('page', 1, type=int)
-    pagination = user.followed.paginate(
+    pagination1=user.followed.paginate(
         page, per_page=current_app.config['FOLLOWERS_PER_PAGE'],
         error_out=False)
-    follows = [{'user': item.followed, 'timestamp': item.timestamp}
-               for item in pagination.items]
-    return render_template('followers.html', user=user, title="Followed by",
-                           endpoint='.followed_by', pagination=pagination,
-                           follows=follows)
+    follows = [{'user': item.follower, 'timestamp': item.timestamp}
+                for item in pagination.items]
+    followeds=[{'user': item.followed, 'timestamp': item.timestamp}
+               for item in pagination1.items]
+    if not followeds:
+        print(1)
+    return render_template('followers.html', user=user, title="Followers of",
+                            endpoint='.followers', pagination=pagination,pagination1=pagination1,
+                            follows=follows,followeds=followeds)
+
+
 
 @users.route("/register",methods=['GET','POST'])
 def register():
