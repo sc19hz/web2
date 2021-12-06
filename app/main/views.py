32c1646f4c80a1bf6238447e231e95c8a01571d2
sc19hz <sc19hz@leeds.ac.uk>
2021-12-06 18:00:46 +0800

    v1.6:fixed no-login error

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,19 @@
 from app.models import Post
-from flask import  request, render_template,Blueprint
-from flask_login import current_user
+from flask import  request, render_template,Blueprint,Flask, url_for
+from flask_login import current_user,login_required
 main=Blueprint('main',__name__)
 from  run import app
 @main.route("/")
 @main.route("/index")
 def index():
+    background=''
+    if current_user.is_authenticated:
+        background=url_for('static',filename=f'picture/{current_user.background}')
     app.logger.info('info log')
-
     app.logger.warning('warning log')
     pagenumber=request.args.get('page',1,type=int)
     posts = Post.query.order_by(Post.postdate.desc()).paginate(page=pagenumber, per_page=4)
-    return render_template("index.html", posts=posts)
+    return render_template("index.html", posts=posts,background=background,tag=1)
 
 @main.route("/more")
 def more():
